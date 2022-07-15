2346022b977bd5f841c1fcdaf81c2dabb709887f
unknown <keanulrich.gozon@gmail.com>
2022-07-15 20:27:06 +0800

    first commit

---
+++ src/components/ListItems.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const ListItems = ({ note }) => {
+    let getTime = (note) => {
+        let date = new Date(note.updated).toLocaleDateString() + ' ' + new Date(note.updated).toLocaleTimeString()
+
+        return date
+    }
+
+    let getTitle = (note) => {
+        let title = note.body.split('\n')[0]
+        if (title.length > 45) {
+            return title.slice(0, 45)
+        }
+        return title
+    }
+
+    let getContent = (note) => {
+        let title = getTitle(note)
+        let content = note.body.replaceAll('\n', ' ')
+        content = content.replaceAll(title, ' ')
+
+        if (content.length > 45) {
+            return content.slice(0, 45) + '...'
+        } else {
+            return title
+        }
+    }
+
+    return (
+
+        <Link to={`/note/${note.id}`}>
+            <div className='notes-list-item'>
+                <h3>{getTitle(note)}</h3>
+                <p>{getContent(note)}<br></br><span>{getTime(note)}</span></p>
+            </div>
+        </Link>
+
+    )
+}
+
+export default ListItems
